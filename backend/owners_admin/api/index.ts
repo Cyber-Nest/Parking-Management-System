7e0a32749b9a11fd6143e4e0fa185e67a57235ee
Yogesh Gadhewal <yogeshgadhewal28@gmail.com>
2026-05-29 19:40:13 +0530

    production issue fix.

--- backend/owners_admin/api/index.ts
+++ backend/owners_admin/api/index.ts
@@ -1,30 +1,3 @@
-// import serverless from "serverless-http";
-
-// import app from "../src/app";
-// import { connectDatabase } from "../src/config/database";
-
-// let isConnected = false;
-
-// const handler = serverless(app);
-
-// export default async (req: any, res: any) => {
-//   if (!isConnected) {
-//     await connectDatabase();
-//     isConnected = true;
-//   }
-
-//   return handler(req, res);
-// };
-
-// import serverless from "serverless-http";
-// import app from "../src/app";
-
-// const handler = serverless(app);
-
-// export default async (req: any, res: any) => {
-//   return handler(req, res);
-// };
-
 import app from "../src/app";
 import { connectDatabase } from "../src/config/database";
 
