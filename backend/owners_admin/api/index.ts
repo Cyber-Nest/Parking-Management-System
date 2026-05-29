0ef80b0db89f78c331f899e804933a6917f3f157
Yogesh Gadhewal <yogeshgadhewal28@gmail.com>
2026-05-29 19:12:50 +0530

    production: connection error fix.

--- backend/owners_admin/api/index.ts
+++ backend/owners_admin/api/index.ts
@@ -1,17 +1,26 @@
+// import serverless from "serverless-http";
+
+// import app from "../src/app";
+// import { connectDatabase } from "../src/config/database";
+
+// let isConnected = false;
+
+// const handler = serverless(app);
+
+// export default async (req: any, res: any) => {
+//   if (!isConnected) {
+//     await connectDatabase();
+//     isConnected = true;
+//   }
+
+//   return handler(req, res);
+// };
+
 import serverless from "serverless-http";
-
 import app from "../src/app";
-import { connectDatabase } from "../src/config/database";
-
-let isConnected = false;
 
 const handler = serverless(app);
 
 export default async (req: any, res: any) => {
-  if (!isConnected) {
-    await connectDatabase();
-    isConnected = true;
-  }
-
   return handler(req, res);
 };
